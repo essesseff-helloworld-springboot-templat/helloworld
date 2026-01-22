b999755d34ec6982918b3aa4cbd8b3cbea765b7a
adamdurst <Adam.durst@gmail.com>
2026-01-22 15:13:42 -0500

    Update HelloWorldApplication.java

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,11 +2,34 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class HelloWorldApplication {
+    
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldApplication.class);
+    
+    @Value("${server.port}")
+    private String port;
+    
+    @Value("${app.environment}")
+    private String environment;
+    
+    @Value("${app.version}")
+    private String version;
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
+    }
+    
+    @EventListener(ApplicationReadyEvent.class)
+    public void logStartup() {
+        logger.info("Starting Hello World Spring Boot server on port {}", port);
+        logger.info("Environment: {}", environment);
+        logger.info("Version: {}", version);
     }
 }
